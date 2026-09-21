36751eac8b98ad4010384d5c1ddc429aa63321a5
jcd1141 <joshd3887@gmail.com>
2026-09-21 22:43:05 +0000

    Added another option, Near Earth asteroids by date

--- nasa.js
+++ nasa.js
@@ -8,7 +8,8 @@ console.log("Welcome to the NASA Explorer!");
 
 console.log("What would you like to do?");
 console.log("1. Astronomy Picture of the Day");
-console.log("2. Exit");
+console.log("2. Near-Earth Asteroids by Date");
+console.log("3. Exit");
 
 let user = prompt("Choice:");
 
@@ -24,6 +25,19 @@ if(user == 1){
     console.log(`Explanation: ${data.explanation}`);
 }
 else if(user == 2){
+    let date = prompt("Enter a date (YYYY-MM-DD):");
+
+    const response = await fetch(
+        `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    let asteroids = data.near_earth_objects[date];
+
+    console.log(`NASA found ${asteroids.length} near-Earth ateroids on ${date}.`);
+}
+else if(user == 3){
     console.log("Have a good day!");
 }
 else{
