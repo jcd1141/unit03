98099c4b0910228842e1984062f8d8f1679087b1
jcd1141 <joshd3887@gmail.com>
2026-09-21 22:35:15 +0000

    Added some prompts

--- nasa.js
+++ nasa.js
@@ -6,13 +6,27 @@ const API_KEY = "DEMO_KEY";
 
 console.log("Welcome to the NASA Explorer!");
 
-const response = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
-);
+console.log("What would you like to do?");
+console.log("1. Astronomy Picture of the Day");
+console.log("2. Exit");
 
-const data = await response.json();
+let user = prompt("Choice:");
 
-console.log(`Title: ${data.title}`);
-console.log(`Date: ${data.date}`);
-console.log(`Explanation: ${data.explanation}`);
+if(user == 1){
+    const response = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+    );
+
+    const data = await response.json();
+
+    console.log(`Title: ${data.title}`);
+    console.log(`Date: ${data.date}`);
+    console.log(`Explanation: ${data.explanation}`);
+}
+else if(user == 2){
+    console.log("Have a good day!");
+}
+else{
+    console.log("Invalid");
+}
 
